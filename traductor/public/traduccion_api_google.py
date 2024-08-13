697822f208d6128b7ecf30b7080cc6e7c0df01e1
Eddu10 <omarmosquera1998@gmail.com>
2024-08-12 22:27:05 -0500

    soporte traduccion api de google

--- traductor/public/traduccion_api_google.py
+++ traductor/public/traduccion_api_google.py
@@ -1,26 +1,14 @@
-from google.cloud import translate_v2 as translate
+from googletrans import Translator
 
-def traducir(texto, idioma_origen, idioma_destino):
-    #Traduce un texto utilizando la API de Google Cloud Translation.
-
-    """Args:
-        texto: "El texto a traducir".
-        idioma_origen: español
-        idioma_destino: ingles.
-
-    Returns:
-        El texto traducido."""
+def traducir_espanol_ingles(mensaje):
+    
+    traductor = Translator()
+    
+    traduccion = traductor.translate(mensaje, src='es', dest='en')
+    
+    return print(traduccion.text)
 
 
-    # Crear un cliente de traducción
-    translate_client = translate.Client()
-
-    # Construir la solicitud de traducción
-    result = translate_client.translate(
-        texts=[texto],
-        source_language=idioma_origen,
-        target_language=idioma_destino
-    )
-
-    # Obtener el texto traducido
-    return result['translations'][0]['translatedText']
+print("Mensaje: \n")
+mensaje = input()
+traducir_espanol_ingles(mensaje)
